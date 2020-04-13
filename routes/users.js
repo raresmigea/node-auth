d70f50b8da16e0349ff605aa0841cf57183679b6
raresmigea <raresini@yahoo.com>
2020-04-13 23:42:15 +0300

    added nodemon and updated express.Router()

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 //const router = require('express-promise-router')();	//it's better than previous because it wraps the code in try-catch by itself
 
 const UserController = require('../controllers/users');
