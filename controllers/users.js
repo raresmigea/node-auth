147db983a7e3a2b933ade515313b4b9d538923eb
raresmigea <raresini@yahoo.com>
2020-04-16 21:25:39 +0300

    server side validation

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,20 @@
 module.exports = {
+	//Server side validation:
+	//we want to validate data before it gets passes to these functions
+	//we create a helper folder - routerHelpers - in this file, create functions
+	//that will use the schema we will provide
 	signUp: async (req, res, next) => {
+		//will expect to get email & pasword from user
+		console.log('contents of req.value.body:', req.value.body);
 		console.log('signUp');
 	},
 	signIn: async (req, res, next) => {
+		//here it will be done by Passport.js - will validate the user
+		//generate token
 		console.log('signIn');
 	},
 	secret: async (req, res, next) => {
+		//no validations here
 		console.log('secret');
 	}
 }
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,22 @@ const express = require('express');
 const router = express.Router();
 // const router = require('express-promise-router')();	//it's better than previous because it wraps the code in try-catch by itself
 
+const { validateBody, schemas } = require('../helpers/routerHelpers');
 const UserController = require('../controllers/users');
 
-router.route('./signup')
-	.post(UserController.signUp);
+//client makes a request - will be a body with data
+//will call validateBody - if it is ok
+//will call it in controllers/signup
+//if it's not ok - will return 400 and controller will not be called
+router.route('/signup')
+	.post(validateBody(schemas.authSchema), UserController.signUp);
 
-router.route('./signin')
+//done by Passport.js
+router.route('/signin')
 	.post(UserController.signIn);
 
-router.route('./secret')
+//no need of validation here
+router.route('/secret')
 	.get(UserController.secret);
 
 module.exports = router;
