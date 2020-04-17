f6037a5c87c5b67bd57ae98956e74838fcc9d51c
raresmigea <raresini@yahoo.com>
2020-04-17 23:16:46 +0300

    moved the nodejs part into server folder

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 
-//connect to database
-mongoose
-  .connect('mongodb+srv://raresmigea:notsodark@cluster0-ptmqz.mongodb.net/test',
-    { useNewUrlParser: true, useUnifiedTopology: true },
-  )
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+const app = require('./server/app');
 
-const app = express();
-
-//middlewares
-//when a request will hit the middlewares, shows it in terminal - morgan, parse it - bodyParser
-app.use(morgan('dev'));			//what format should sync inside the terminal
-app.use(bodyParser.json());		//must parse json
-
-//routes
-app.use('/users', require('./routes/users'));
-
-//start the server
-
-const port = process.env.PORT || 3000;
+// Start the server
+const port = process.env.PORT || 5000;
 app.listen(port);
-console.log(`Server is listening at ${port}`);
+console.log(`Server listening at ${port}`);
+
+// refactored code for easier test and feature scale
---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+// mongoose.Promise = global.Promise;
+//connect to database
+mongoose
+  .connect('mongodb+srv://raresmigea:notsodark@cluster0-ptmqz.mongodb.net/test',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const app = express();
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  })
+);
+
+// Middlewares moved morgan into if for clear tests
+if (!process.env.NODE_ENV === "test") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+// Routes
+app.use("/users", require("./routes/users"));
+
+module.exports = app;
