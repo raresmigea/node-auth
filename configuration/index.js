5d86a39d69ea9da17d3da9ded1b74025086a1717
raresmigea <raresini@yahoo.com>
2020-04-17 22:42:11 +0300

    created react app

--- configuration/index.js
+++ configuration/index.js
@@ -1,3 +1,29 @@
-module.exports = {
-  JWT_SECRET: 'mySecretKey'
+if (process.env.NODE_ENV === 'test') {
+  module.exports = {
+    JWT_SECRET: 'mySecretKey',
+    oauth: {
+      google: {
+        clientID: 'number',
+        clientSecret: 'string',
+      },
+      facebook: {
+        clientID: 'number',
+        clientSecret: 'string',
+      },
+    },
+  };
+} else {
+  module.exports = {
+    JWT_SECRET: 'mySecretKey',
+    oauth: {
+      google: {
+        clientID: 'number',
+        clientSecret: 'string',
+      },
+      facebook: {
+        clientID: 'number',
+        clientSecret: 'string',
+      },
+    },
+  };
 }
