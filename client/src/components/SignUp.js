c5d06b4a9cb037c3ebddbe6172fee65417ab260e
raresmigea <raresini@yahoo.com>
2020-04-18 21:52:30 +0300

    integrate redux & redux form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-export default class SignUp extends Component {
+import CustomInput from './CustomInput';
+
+ class SignUp extends Component {
   render() {
     return (
       <div>
-        This is sign up component
+        <form>
+          <fieldset>
+            <Field
+              name="email"
+              type="text"
+              id="email"
+              label="enter your email"
+              placeholder="example@example.com"
+              component={ CustomInput } />
+          </fieldset>
+
+          <fieldset>
+            <Field
+              name="password"
+              type="password"
+              id="password"
+              label="enter your password"
+              placeholder="your secret password"
+              component={ CustomInput } />
+          </fieldset>
+
+          <button type="submit" className="btn btn-primary">Sign Up</button>
+        </form>
       </div>
     );
   }
 }
+
+export default reduxForm({ form: 'signup' })(SignUp)
