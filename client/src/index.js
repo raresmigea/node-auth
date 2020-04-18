181c90159bd9e66b211044c49134a5daa27f13b1
raresmigea <raresini@yahoo.com>
2020-04-18 18:52:31 +0300

    added signUp, signIn & dashboard components

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import Home from './components/Home';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import Dashboard from './components/Dashboard';
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/dashboard" component={Dashboard} />
     </App>
   </BrowserRouter>,
   document.querySelector('#root'));
