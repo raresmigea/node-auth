d6440c4109715463a9ed772134d58f5d45a73fdc
raresmigea <raresini@yahoo.com>
2020-11-01 23:42:36 +0200

    white spaces before comments

--- server/passport.js
+++ server/passport.js
@@ -1,28 +1,28 @@
 import { use } from 'passport';
-import { Strategy as jwtStrategy } from 'passport-jwt'; //sign up
+import { Strategy as jwtStrategy } from 'passport-jwt'; // sign up
 import { ExtractJwt } from 'passport-jwt';
 import { Strategy as LocalStrategy } from 'passport-local'; // sign in
 import { JWT_SECRET } from './configuration';
 import { findById, findOne } from './models/user';
 
-//JWT Strategy
+// JWT Strategy
 use(
   new jwtStrategy(
     {
-      jwtFromRequest: ExtractJwt.fromHeader('authorization'), //where the token will be contained
+      jwtFromRequest: ExtractJwt.fromHeader('authorization'), // where the token will be contained
       secretOrKey: JWT_SECRET, //the secret
     },
     async (payload, done) => {
       try {
-        //take the data from the payload, find the user and return it
+        // take the data from the payload, find the user and return it
         const user = await findById(payload.sub);
 
-        //if user doesn't exist - handle it
+        // if user doesn't exist - handle it
         if (!user) {
           return done(null, false);
         }
 
-        //otherwise - return the user
+        // otherwise - return the user
         done(null, user);
       } catch (error) {
         done(error, false);
@@ -31,7 +31,7 @@ use(
   )
 );
 
-//LOCAL Strategy
+// LOCAL Strategy
 use(
   new LocalStrategy(
     {
@@ -40,21 +40,21 @@ use(
     },
     async (email, password, done) => {
       try {
-        //find the user by email
+        // find the user by email
         const user = await findOne({ email: email });
 
-        //if not - handle it
+        // if not - handle it
         if (!user) {
           return done(null, false);
         }
-        //check if the password is correct
+        // check if the password is correct
         const isMatch = await user.isValidPassword(password);
 
-        //if not - handle it
+        // if not - handle it
         if (!isMatch) {
           return done(null, false);
         }
-        //return the user
+        // return the user
         done(null, user);
       } catch (error) {
         done(error, false);
